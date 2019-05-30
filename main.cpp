46d177907fb8dfbe71cdaaf80590ce8ecaccc209
Jacques Lucke <mail@jlucke.com>
2019-05-30 09:49:48 +0200

    renaming

--- main.cpp
+++ main.cpp
@@ -7,30 +7,31 @@
 #include <sstream>
 #include <string>
 
-struct VecF4 {
+class fvec4 {
   __m128 m_value;
 
-  VecF4(__m128 v) : m_value(v) {}
-  VecF4(float v) { m_value = _mm_set_ps1(v); }
-  VecF4(float a, float b, float c, float d) {
+ public:
+  fvec4(__m128 v) : m_value(v) {}
+  fvec4(float v) { m_value = _mm_set_ps1(v); }
+  fvec4(float a, float b, float c, float d) {
     m_value = _mm_set_ps(a, b, c, d);
   }
 
   __m128 m128() const { return m_value; }
 
-  friend VecF4 operator+(const VecF4 &a, const VecF4 &b) {
+  friend fvec4 operator+(const fvec4 &a, const fvec4 &b) {
     return _mm_add_ps(a.m128(), b.m128());
   }
 
-  friend VecF4 operator*(const VecF4 &a, const VecF4 &b) {
+  friend fvec4 operator*(const fvec4 &a, const fvec4 &b) {
     return _mm_mul_ps(a.m128(), b.m128());
   }
 
-  friend VecF4 operator-(const VecF4 &a, const VecF4 &b) {
+  friend fvec4 operator-(const fvec4 &a, const fvec4 &b) {
     return _mm_sub_ps(a.m128(), b.m128());
   }
 
-  friend std::ostream &operator<<(std::ostream &stream, const VecF4 &v) {
+  friend std::ostream &operator<<(std::ostream &stream, const fvec4 &v) {
     char data[128];
     snprintf(data, sizeof(data), "(%.5f, %.5f, %.5f, %.5f)", v.get<0>(),
              v.get<1>(), v.get<2>(), v.get<3>());
@@ -46,8 +47,8 @@ struct VecF4 {
 };
 
 int main(int argc, char const *argv[]) {
-  VecF4 a{3, 4, 5, 2};
-  VecF4 b{7, 8, 2, 3};
+  fvec4 a{3, 4, 5, 2};
+  fvec4 b{7, 8, 2, 3};
   std::cout << "a " << a << "\n";
   std::cout << "b " << b << "\n";
   std::cout << "R " << (a * b) << "\n";
